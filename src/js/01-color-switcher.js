fdbcdf934dc026fbd376ea72641050ce5d6f30d7
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-28 22:49:57 +0300

    task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let colorChangeId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClickColorChange);
+refs.stopBtn.addEventListener('click', onStopBtnClickColorChangeStop);
+
+function onStartBtnClickColorChange() {
+    colorChangeId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor(); }, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+};
+
+function onStopBtnClickColorChangeStop() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(colorChangeId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
