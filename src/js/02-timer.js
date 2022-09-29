8621ac72a18dcbc17de08bf37c4d5e31e280a0b2
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-29 10:53:26 +0300

    disables time picker then the timer is on

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,35 +1,33 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-    startBtn: document.querySelector('button[data-start]'),
-    timer: document.querySelector('.timer'),
-    fields: document.querySelectorAll('.field'),
-    values: document.querySelectorAll('.value'),
-    labels: document.querySelectorAll('.label'),
+  startBtn: document.querySelector('button[data-start]'),
+  timer: document.querySelector('.timer'),
+  fields: document.querySelectorAll('.field'),
+  values: document.querySelectorAll('.value'),
+  labels: document.querySelectorAll('.label'),
 };
 
 refs.timer.style.display = 'flex';
-
 refs.timer.style.gap = '20px';
 
 refs.fields.forEach(field => {
-    field.style.display = 'flex';
-    field.style.flexDirection = 'column';
-    field.style.alignItems = 'center';
+  field.style.display = 'flex';
+  field.style.flexDirection = 'column';
+  field.style.alignItems = 'center';
 });
 
-refs.values.forEach(value => value.style.fontSize = '50px');
+refs.values.forEach(value => (value.style.fontSize = '50px'));
 
 refs.labels.forEach(label => {
-    label.style.fontSize = '15px';
-    label.style.textTransform = 'uppercase';
-    label.style.fontWeight = '600';
+  label.style.fontSize = '15px';
+  label.style.textTransform = 'uppercase';
+  label.style.fontWeight = '600';
 });
 
 refs.startBtn.disabled = true;
-
 let choosenDate = 0;
 let timerId = null;
 
@@ -41,31 +39,38 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    if (!refs.timer.classList.contains('active')) {
       if (Date.now() - selectedDates[0] > 0) {
-          Notify.failure('Please choose a date in the future!');
-          return;
-      };
+        Notify.failure('Please choose a date in the future!');
+        return;
+      }
       refs.startBtn.disabled = false;
-      return choosenDate = selectedDates[0];
+      refs.timer.classList.add('active');
+      return (choosenDate = selectedDates[0]);
+    }
   },
 };
 
 flatpickr('input[type="text"]', options);
 
 function onStartBtnClick() {
-    refs.startBtn.disabled = true;
-    timerId = setInterval(timer, 1000);
-};
+  timerId = setInterval(timer, 1000);
+}
 
 function timer() {
-    const time = choosenDate - Date.now();
-    if (time <= 0) {
-        clearInterval(timerId);
-        return;
-    };
-    const obj = convertMs(time);
-    refs.values.forEach((value, i) => value.textContent = addLeadingZero(Object.values(obj)[i])); 
-};
+  refs.startBtn.disabled = true;
+  const time = choosenDate - Date.now();
+  if (time <= 0) {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.timer.classList.remove('active');
+    return;
+  }
+  const obj = convertMs(time);
+  refs.values.forEach(
+    (value, i) => (value.textContent = addLeadingZero(Object.values(obj)[i]))
+  );
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -84,8 +89,8 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-};
+}
 
 function addLeadingZero(value) {
-    return value.toString().padStart(2, 0);
-};
+  return value.toString().padStart(2, 0);
+}
