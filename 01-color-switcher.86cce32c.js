9926004a03b17f23b283a564752588d566ce50f7
anvomm <104103036+anvomm@users.noreply.github.com>
2022-09-29 12:05:53 +0000

    Deploying to gh-pages from @ anvomm/goit-js-hw-09@9b62c8b055ea5323e4003d2f5f95d98378da1224 🚀

---
+++ 01-color-switcher.86cce32c.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;function e(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startBtn.addEventListener("click",(function(){document.body.style.backgroundColor=e(),n=setInterval((()=>document.body.style.backgroundColor=e()),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.86cce32c.js.map
